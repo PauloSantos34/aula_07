b59195b3ad2e444d82c0f783ef7530cebb590021
PauloSantos34 <paulovasc705@outlook.com>
2025-04-29 21:27:20 -0300

    funções dobro e triplo

---
+++ aula_07/atividade01.py
@@ -0,0 +1,16 @@
+def multiplicar_numeros():
+    
+    qtde = float(input('Informe a quantidade solicitada: '))
+
+    dobro = qtde *2 
+    triplo = qtde *3
+    quadrado = qtde **2
+
+    print(f"{dobro} {triplo} {quadrado}")
+
+multiplicar_numeros()  
+
+
+   
+
+ 
